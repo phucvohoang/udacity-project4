b6ab51d773e12d2e52541aa7bdffddc3ad175fc8
phucvo <phucvoaff@gmail.com>
2023-06-03 08:44:46 +0700

    added XRAY on DynamoDB, serverless

--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -47,6 +47,11 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
   onTodoCreate = async (event: React.ChangeEvent<HTMLButtonElement>) => {
     try {
       const dueDate = this.calculateDueDate()
+      if (!this.state.newTodoName) {
+        // Missing todoname, should warn user
+        alert('Please provide todoname')
+        return // do nothing
+      }
       const newTodo = await createTodo(this.props.auth.getIdToken(), {
         name: this.state.newTodoName,
         dueDate
