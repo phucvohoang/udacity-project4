fdfd7702ef253dd9d3043d281295c931ad66f080
phucvo <phucvoaff@gmail.com>
2023-06-04 22:14:43 +0700

    remove global iam role, update new appId for testing

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'o2brbx3joj'
+const apiId = 'h2tc3ls49k'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
